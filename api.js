190b0824bcc44d8e3bdb92fe03aaeef2f5d10320
Clark Alms <clark.alms@gmail.com>
2019-06-11 18:09:44 -0500

    completed api.js file, added addReservations, getReservations, and clearReservations functions

--- api.js
+++ api.js
@@ -1,115 +1,51 @@
-var mysql = require("mysql");
+const mysql = require("mysql");
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host: "localhost",
-
-  // Your port; if not 3306
   port: 8889,
-
-  // Your username
   user: "root",
-
-  // Your password
   password: "root",
   database: "tabledb"
 });
 
-// var newReso = {
-//   name: $("#inputName").val().trim(),
-//   number: $("#inputPhone").val().trim(),
-//   email: $("#inputEmail").val().trim(),
-// };
-
-// $("#add-btn").on("click", function (event) {
-//   event.preventDefault();
-
-//   $.post("/api/reservations", newReso)
-//     .then(function (data) {
-//       console.log(data);
-//       alert("Adding reservation...");
-//     });
-
-//     dbConnect();
-
-// });
-
-  connection.connect(function (err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
-    // addTable();
+function addReservations(request, response) {
+  connection.connect(function (error) {
+    connection.query("INSERT INTO customer_info SET ?",
+      {
+        first_name: request.body.first_name,
+        last_name: request.body.last_name,
+        email: request.body.email,
+        phone_number: request.body.phone_number
+      },
+      // TODO: Understand this code... Spread operator vs. rest parameters?
+      function (error, rows) {
+        let reservation = { ...request.body };
+        reservation.customer_id = rows.insertId;
+        response.json(reservation);
+      });
   });
+};
 
-
-
-function addTable() {
-  console.log("Inserting customers in DB...\n");
-  var query = connection.query(
-    "INSERT INTO customer_info SET ?",
-    {
-      customerName: newReso.name,
-      customerEmail: newReso.email,
-      phoneNumber: newReso.number
-    },
-    function (err, res) {
-      console.log(res.affectedRows + " table added!");
-      // Call updateProduct AFTER the INSERT completes
-      //   updateProduct();
-      readDB();
-    }
-  );
-
-  // logs the actual query being run
-  console.log(query.sql);
-}
-
-// function updateProduct() {
-//   console.log("Updating all Rocky Road quantities...\n");
-//   var query = connection.query(
-//     "UPDATE products SET ? WHERE ?",
-//     [
-//       {
-//         quantity: 100
-//       },
-//       {
-//         flavor: "Rocky Road"
-//       }
-//     ],
-//     function(err, res) {
-//       console.log(res.affectedRows + " products updated!\n");
-//       // Call deleteProduct AFTER the UPDATE completes
-//       deleteProduct();
-//     }
-//   );
-
-//   // logs the actual query being run
-//   console.log(query.sql);
-// }
-
-// function deleteProduct() {
-//   console.log("Deleting all strawberry icecream...\n");
-//   connection.query(
-//     "DELETE FROM products WHERE ?",
-//     {
-//       flavor: "strawberry"
-//     },
-//     function(err, res) {
-//       console.log(res.affectedRows + " products deleted!\n");
-//       // Call readProducts AFTER the DELETE completes
-//       readProducts();
-//     }
-//   );
-// }
-
-function readDB() {
-  console.log("Selecting all customers...\n");
-  connection.query("SELECT * FROM customer_info", function (err, res) {
-    if (err) throw err;
-    // Log all results of the SELECT statement
-    console.log(res);
-    // connection.end();
+function getReservations(request, response) {
+  connection.connect(function (error) {
+    connection.query("SELECT * FROM customer_info", function (error, rows) {
+      response.json(rows)
+    });
   });
-}
+};
+
+function clearReservations(request, response){
+  connection.connect(function(error){
+    connection.query("TRUNCATE TABLE customer_info", function(error, rows){
+      response.json({
+        'message': 'reservations cleared.'
+      });
+    });
+  });
+};
 
 module.exports = {
-  readDB: readDB
-}
+  getReservations: getReservations,
+  addReservations: addReservations,
+  clearReservations: clearReservations
+};
